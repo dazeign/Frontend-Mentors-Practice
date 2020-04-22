8245729f2c584d75a7e91ac3c05a6b029779decb
Frankpeter Ani <frankpeterani@gmail.com>
2020-04-22 22:11:33 +0100

    Tailwind isnt all that flexible

--- DazeFrontendMasters/tailwind.config.js
+++ DazeFrontendMasters/tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tradewind: {
+        100: '#EFF8F7',
+        200: '#D6ECEB',
+        300: '#BDE1DF',
+        400: '#8CCBC8',
+        500: '#5BB4B0',
+        600: '#52A29E',
+        700: '#376C6A',
+        800: '#29514F',
+        900: '#1B3635',
+        }
+      }  
+    } 
   },
   variants: {},
   plugins: [],
